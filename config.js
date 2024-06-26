f88d22eb2209129e32a1563be0a2b8f3cc739f4d
bhargavyellina <bhargavyellina@gmail.com>
2024-06-26 12:03:10 +0530

    modified link

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-var actionLink = "https://script.google.com/u/0/home/projects/15eRE98t_pjJqXtH2FKhnER0QL_gYBoci-GVXmOpc_C5VX2KGCCKJZ4nT/edit";
+var actionLink = "https://script.google.com/macros/s/AKfycbynGbzC7y73xbjYAepXMyqMb-Dg83BKpuPIWCpeTQyPI2fVJSYMfOX-i54k-o78Z9kS6Q/exec";
