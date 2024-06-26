ed5278820db3d63de09e2ae36c36526d7ec797b5
bhargavyellina <bhargavyellina@gmail.com>
2024-06-26 15:58:01 +0530

    modified config.js added links

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 // config.js
 var actionLink = "https://script.google.com/macros/s/AKfycbynGbzC7y73xbjYAepXMyqMb-Dg83BKpuPIWCpeTQyPI2fVJSYMfOX-i54k-o78Z9kS6Q/exec";
+
+var spreadsheetlink = "https://docs.google.com/spreadsheets/d/1PX85JfbKAsnLeNZxox6iolH5n7HZFgIHAQYxylAmn44/edit?gid=1019489874#gid=1019489874";
+var appsscriptlink = "https://script.google.com/u/0/home/projects/15eRE98t_pjJqXtH2FKhnER0QL_gYBoci-GVXmOpc_C5VX2KGCCKJZ4nT/edit"
